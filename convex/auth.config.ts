dcefb43d8052fcafd8ee48325117b6567a448f68
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2024-06-28 18:19:33 +0700

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://cute-skunk-44.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
